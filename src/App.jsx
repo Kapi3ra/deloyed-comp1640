e580bec73364d372e2d7f5017b092e1ba7215a80
Tĩnh Kapi <tinhtue111@gmail.com>
2024-11-10 15:28:19 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Thêm Navigate
 
 import Layout from './Layout';
 import Home from './Components/Home/Home';
@@ -9,7 +9,7 @@ import LoginManager from './Components/Login/LoginManager';
 
 import TeacherDashboard from './Components/Teacher/TeacherDashboard';
 import StudentDashboard from './Components/Student/StudentDashboard';
-import StudentAdd from './Components/Student/StudentAdd'; // Import StudentAdd
+import StudentAdd from './Components/Student/StudentAdd';
 import SupervisorDashboard from './Components/Supervisor/SupervisorDashboard';
 import GuestDashboard from './Components/Guest/GuestDashboard';
 import PageNotFound from './Components/PageNotFound/PageNotFound';
@@ -25,14 +25,16 @@ import GuestEdit from './Components/Guest/GuestEdit';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/deloyed-comp1640"> {/* Thêm basename */}
       <Routes>
+        {/* Chuyển hướng gốc ("/") đến trang đăng nhập */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
         {/* Route cho trang đăng nhập không có layout */}
         <Route path="/login" element={<Login />} />
-        <Route index element={<Login />} />
+        
         {/* Route cho các trang khác sẽ bao gồm Layout */}
         <Route path="/" element={<Layout />}>
-          
           <Route path="loginadd" element={<LoginAdd />} />
           <Route path="loginmanager" element={<LoginManager />} />
           
@@ -45,14 +47,13 @@ function App() {
           <Route path="new-subject" element={<NewSubject />} />
           <Route path="supervisor-dashboard" element={<SupervisorDashboard />} />
           <Route path="guest-dashboard" element={<GuestDashboard />} />
-          <Route path="subject" element={<Subject />} />
-          <Route path="new-subject" element={<NewSubject />} />
           <Route path="department-dashboard" element={<DepartmentDashboard />} />
           <Route path="department-add" element={<DepartmentAdd />} />
-          <Route path="department-edit/:id" element={<DepartmentEdit />} /> {/* Chỉnh sửa đường dẫn cho DepartmentEdit */}
+          <Route path="department-edit/:id" element={<DepartmentEdit />} />
           <Route path="guestadd" element={<GuestAdd />} />
           <Route path="guestmanager" element={<GuestManager />} />
           <Route path="/guest-edit/:id" element={<GuestEdit />} />
+          
           {/* Xử lý trang không tìm thấy */}
           <Route path="*" element={<PageNotFound />} />
         </Route>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/deloyed-comp1640/login',  // Đảm bảo đây là đường dẫn chính xác của dự án trên GitHub Pages
+  base: '/deloyed-comp1640/', // Đảm bảo rằng base khớp với URL dự án GitHub Pages của bạn
 })
