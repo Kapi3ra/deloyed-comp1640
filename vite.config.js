87206ebe1f1c79d16efa308f8806b620edba72ae
Tĩnh Kapi <tinhtue111@gmail.com>
2024-11-10 15:05:48 +0700

    update: student

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/deloyed-comp1640/login',  // Đảm bảo đây là đường dẫn chính xác của dự án trên GitHub Pages
 })
